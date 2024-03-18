98bd4ef09b68840387e48319e9507fcdd278b69e
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-03-18 14:59:34 -0700

    Se inicia el diseño de la interfaz

---
+++ Licencias_interfaz/src/main/java/com/mycompany/licencias_interfaz/Licencias_interfaz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.licencias_interfaz;
+
+/**
+ *
+ * @author jesus
+ */
+public class Licencias_interfaz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
